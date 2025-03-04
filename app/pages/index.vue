17cb1cd58053ca4d9f9ef6367414fe906ed68785
Nemanja Dragun <39500267+ndragun92@users.noreply.github.com>
2025-03-04 23:24:08 +0100

    Initial commit

---
+++ app/pages/index.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="p-8 text-center">
+    <NuxtImg class="block mx-auto w-12" src="/favicon.png" />
+    <h1 class="font-bold mt-12 text-center">
+      Welcome to Nuxt 3 Starter Template Kit
+    </h1>
+    <h2 class="font-semibold mb-2 mt-10">
+      Included Modules:
+    </h2>
+    <ul class="flex flex-col gap-2">
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="https://nuxt.com/modules/security"
+          target="_blank"
+        >nuxt-security</a>
+      </li>
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="https://pinia.vuejs.org/ssr/nuxt.html"
+          target="_blank"
+        >@pinia/nuxt</a>
+      </li>
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="https://tailwindcss.com"
+          target="_blank"
+        >
+          tailwindcss
+        </a>
+      </li>
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="https://github.com/nuxt-modules/icon"
+          target="_blank"
+        >nuxt-icon</a>
+      </li>
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="https://vueuse.org/guide/#nuxt"
+          target="_blank"
+        >@vueuse/nuxt</a>
+      </li>
+      <li>
+        <a
+          class="hover:underline text-blue-500"
+          href="https://image.nuxt.com"
+          target="_blank"
+        >@nuxt/image</a>
+      </li>
+      <li class="mt-12">
+        <a
+          class="flex gap-2 items-center justify-center"
+          href="https://github.com/ndragun92/template-nuxt3"
+          target="_blank"
+        >
+          <Icon size="24" name="mdi:github" /> Click here to check GitHub
+          Repository
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>

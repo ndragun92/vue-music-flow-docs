354872efe85317926633a38e77c98ac77b08f9ed
ndragun92 <nemanjadragun92@gmail.com>
2025-03-06 11:28:25 +0100

    feat: create reusable terminal component and integrate it
    Add a new `ElTerminal` component for terminal-like interfaces with slots for dynamic content. Introduced `--animate-blink` CSS utility for animations. Replaced hardcoded terminal UI in the installation page with `ElTerminal` for better reusability and maintainability.

---
+++ app/components/ElTerminal/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    class="shadow-lg shadow-zinc-950/25 text-zinc-100 text-sm font-mono subpixel-antialiased bg-zinc-800 rounded-lg leading-normal overflow-hidden"
+  >
+    <div class="flex items-center gap-2 mb-2 bg-zinc-950 px-4 py-2">
+      <div class="flex space-x-2">
+        <div class="size-3 bg-red-500 rounded-full" />
+        <div class="size-3 bg-orange-300 rounded-full" />
+        <div class="size-3 bg-green-500 rounded-full" />
+      </div>
+      <div class="flex-1 text-center min-w-0">
+        <slot name="file" />
+      </div>
+    </div>
+    <div class="mt-4 flex flex-col px-5 pb-6 pt-4">
+      <slot name="content">
+        <div
+          v-for="(command, commandIndex) in prop.commands"
+          :key="`${command}-${commandIndex}`"
+          class="flex items-baseline gap-1 flex-wrap"
+        >
+          <span class="text-emerald-500">computer:~$</span>
+          <p class="flex-1 pl-2 flex items-center">
+            {{ command }}
+            <Icon
+              v-if="prop.commands.length === commandIndex + 1"
+              name="ph:line-vertical"
+              class="text-white animate-blink"
+            />
+          </p>
+        </div>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  commands: string[];
+};
+
+const prop = withDefaults(defineProps<Props>(), {
+  commands: () => [],
+});
+</script>
--- app/pages/installation/index.vue
+++ app/pages/installation/index.vue
@@ -1,25 +1,9 @@
 <template>
   <h2>Installation</h2>
   <div class="w-full">
-    <div
-      class="shadow-lg text-zinc-100 text-sm font-mono subpixel-antialiased bg-zinc-800 rounded-lg leading-normal overflow-hidden"
-    >
-      <div class="flex items-center gap-2 mb-2 bg-zinc-950 px-4 py-2">
-        <div class="flex space-x-2">
-          <div class="size-3 bg-red-500 rounded-full" />
-          <div class="size-3 bg-orange-300 rounded-full" />
-          <div class="size-3 bg-green-500 rounded-full" />
-        </div>
-        <div class="flex-1 text-center min-w-0">nuxt.config.ts</div>
-      </div>
-      <div class="mt-4 flex px-5 pb-6 pt-4">
-        <span class="text-green-400">computer:~$</span>
-        <p class="flex-1 items-center pl-2">
-          apt-get hire me
-          <br />
-        </p>
-      </div>
-    </div>
+    <ElTerminal :commands="['npm install npm install', 'npm run dev']">
+      <template #file>Terminal</template>
+    </ElTerminal>
   </div>
 </template>
 <script setup lang="ts"></script>

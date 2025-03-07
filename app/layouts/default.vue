31d4f2a5d3f5b1bafd9bb555bca4d2b49c3e546a
ndragun92 <nemanjadragun92@gmail.com>
2025-03-07 16:11:57 +0100

    refactor: simplify layout classes and remove unused SVG
    Refactored CSS classes in the default layout for improved clarity and consistency by removing unnecessary dark mode-specific styles. The unused SVG element was also removed to streamline the DOM structure and enhance maintainability.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -117,14 +117,14 @@
     </aside>
     <main class="flex-1 min-w-0">
       <div
-        class="absolute pointer-events-none top-0 left-1/2 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]"
+        class="absolute pointer-events-none top-0 left-1/2 ml-[-38rem] h-[25rem] w-[81.25rem] [mask-image:linear-gradient(white,transparent)]"
       >
         <div
-          class="absolute inset-0 bg-linear-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100"
+          class="absolute inset-0 bg-linear-to-r [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] from-[#36b49f]/30 to-[#DBFF75]/30 opacity-100"
         >
           <svg
             aria-hidden="true"
-            class="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-18deg] fill-black/40 stroke-black/50 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/5"
+            class="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-18deg] mix-blend-overlay fill-white/2.5 stroke-white/5"
           >
             <defs>
               <pattern
@@ -152,15 +152,6 @@
             </svg>
           </svg>
         </div>
-        <svg
-          viewBox="0 0 1113 440"
-          aria-hidden="true"
-          class="absolute top-0 left-1/2 ml-[-19rem] w-[69.5625rem] fill-white blur-[26px] dark:hidden"
-        >
-          <path
-            d="M.016 439.5s-9.5-300 434-300S882.516 20 882.516 20V0h230.004v439.5H.016Z"
-          />
-        </svg>
       </div>
       <nav
         class="border-b border-zinc-800 h-14 bg-zinc-950/50 flex justify-between items-center px-8"

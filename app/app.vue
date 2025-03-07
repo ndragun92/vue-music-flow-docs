feea3ab7c770b4e6d3f85be6e68c847d4bdeb0e5
ndragun92 <nemanjadragun92@gmail.com>
2025-03-07 09:37:44 +0100

    feat(app): enhance metadata handling and add social previews
    Added support for dynamic metadata rendering based on app context, including title and description. Introduced meta tags and images for improved social sharing previews. Adjusted protocol handling in the init plugin for better environment compatibility.

--- app/app.vue
+++ app/app.vue
@@ -1,16 +1,27 @@
 <template>
+  <NuxtLoadingIndicator />
   <NuxtLayout>
     <Html lang="en" :data-route-path="$route?.path">
       <Head>
-        <Title>Template</Title>
+        <Title>{{ title }}</Title>
+        <meta name="color-scheme" content="dark" />
         <Meta name="language" content="en" />
-        <Meta property="og:title" content="Template" />
-        <Meta name="twitter:title" content="Template" />
-        <Meta name="description" content="Template" />
-        <Meta property="og:description" content="Template" />
-        <Meta name="twitter:description" content="Template" />
-        <Meta property="og:image" content="/favicon.png" />
-        <Meta name="twitter:image" content="/favicon.png" />
+        <Meta property="og:title" :content="title" />
+        <Meta name="twitter:title" :content="title" />
+        <Meta name="description" :content="description" />
+        <Meta property="og:description" :content="description" />
+        <Meta name="twitter:description" :content="description" />
+        <Meta
+          property="og:image"
+          :content="`${nuxtApp?.$app_origin}/social.jpg`"
+        />
+        <Meta
+          name="twitter:image"
+          :content="`${nuxtApp?.$app_origin}/twitter.jpg`"
+        />
+        <Meta name="twitter:card" content="summary_large_image" />
+        <Meta name="twitter:site" content="@nemanjadragun" />
+        <Meta name="twitter:creator" content="@nemanjadragun" />
         <Meta property="og:site_name" content="/favicon.png" />
         <Meta name="twitter:site" content="/favicon.png" />
         <Meta name="url" :content="`${nuxtApp?.$app_origin}${$route?.path}`" />
@@ -18,9 +29,20 @@
           property="og:url"
           :content="`${nuxtApp?.$app_origin}${$route?.path}`"
         />
-        <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <Link rel="apple-touch-icon" type="image/png" href="/favicon.png" />
-        <Link rel="apple-touch-startup-image" href="/favicon.png" />
+        <Link
+          rel="icon"
+          type="image/x-icon"
+          :href="`${nuxtApp?.$app_origin}/favicon.ico`"
+        />
+        <Link
+          rel="apple-touch-icon"
+          type="image/png"
+          :href="`${nuxtApp?.$app_origin}/favicon.png`"
+        />
+        <Link
+          rel="apple-touch-startup-image"
+          :href="`${nuxtApp?.$app_origin}/favicon.ico`"
+        />
       </Head>
     </Html>
     <NuxtPage />
@@ -28,5 +50,9 @@
 </template>
 
 <script lang="ts" setup>
-const nuxtApp = useNuxtApp()
+const nuxtApp = useNuxtApp();
+
+const title = "Vue Music Flow";
+const description =
+  "Modern Vue 3 / Nuxt 3 audio player component with playlist and waveform visualization";
 </script>
--- app/plugins/init.ts
+++ app/plugins/init.ts
@@ -1,13 +1,17 @@
 export default defineNuxtPlugin((nuxtApp) => {
-  let host = ''
+  let host = "";
   // Get host in case of server side request
   if (import.meta.env.SSR) {
-    host = nuxtApp?.ssrContext?.event?.req?.headers?.host || ''
+    host = nuxtApp?.ssrContext?.event?.req?.headers?.host || "";
   } else {
-    host = window.location.host
+    host = window.location.host;
   }
-  const origin = new URL(`https://${host}`)
+  let protocol = "https";
+  if (process.env.NODE_ENV === "development") {
+    protocol = "http";
+  }
+  const origin = new URL(`${protocol}://${host}`);
   // Extract origin and set data in app context
-  nuxtApp.provide('app_origin', origin.origin)
-  nuxtApp.provide('app_hostname', origin.hostname)
-})
+  nuxtApp.provide("app_origin", origin.origin);
+  nuxtApp.provide("app_hostname", origin.hostname);
+});

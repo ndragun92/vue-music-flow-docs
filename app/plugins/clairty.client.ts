b5059e1247705ef160a3da1d2f414ac293d1aabe
ndragun92 <nemanjadragun92@gmail.com>
2025-03-07 21:50:56 +0100

    feat: conditionally initialize Clarity based on environment
    Clarity is now disabled in development mode to avoid unnecessary analytics during development. It will only initialize in non-development environments, ensuring cleaner debugging.

--- app/plugins/clairty.client.ts
+++ app/plugins/clairty.client.ts
@@ -1,6 +1,10 @@
 import Clarity from "@microsoft/clarity";
 
 export default defineNuxtPlugin(() => {
-  Clarity.init("qko0ej55oc");
-  console.info("Clarity Initialized");
+  if (process.env.NODE_ENV === "development") {
+    console.info("Clarity disabled in development mode");
+  } else {
+    Clarity.init("qko0ej55oc");
+    console.info("Clarity Initialized");
+  }
 });

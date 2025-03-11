0df4d340837dfe3187d60e86fcecc925bf9ea5ad
ndragun92 <nemanjadragun92@gmail.com>
2025-03-11 16:29:35 +0100

    feat: update Nuxt 3 setup with module-based installation
    Revised the usage page to reflect the new module-based Nuxt 3 integration. Updated code examples and removed outdated implementation. Adjusted installation instructions to include both Vue 3 and Nuxt 3 setups.

--- app/pages/usage/index.vue
+++ app/pages/usage/index.vue
@@ -5,13 +5,20 @@
     <ElPrism :code="vueCode">
       <template #file>./src/App.vue</template>
     </ElPrism>
-    <h3 class="text-lg font-medium">Nuxt 3 simple implementation</h3>
-    <ElPrism :code="nuxtConfigCode" :ts="true">
-      <template #file>./nuxt.config.ts</template>
-    </ElPrism>
-    <ElPrism :code="nuxtComponentCode">
-      <template #file>./components/MusicFlow/index.vue</template>
-    </ElPrism>
+    <h3 id="nuxt" class="text-lg font-medium">Nuxt 3 module implementation</h3>
+    <ElTerminal
+      :commands="['npx nuxi module add nuxt-music-flow']"
+      code="npx nuxi module add nuxt-music-flow"
+    >
+      <template #file>Terminal</template>
+    </ElTerminal>
+    <!--    <h3 class="text-lg font-medium">or (non module) implementation</h3>-->
+    <!--    <ElPrism :code="nuxtConfigCode" :ts="true">-->
+    <!--      <template #file>./nuxt.config.ts</template>-->
+    <!--    </ElPrism>-->
+    <!--    <ElPrism :code="nuxtComponentCode">-->
+    <!--      <template #file>./components/MusicFlow/index.vue</template>-->
+    <!--    </ElPrism>-->
     <ElPrism :code="nuxtCode">
       <template #file>./app/pages/index.vue</template>
     </ElPrism>
@@ -40,7 +47,7 @@
 
 <script setup lang="ts">
 import { vueCode } from "public/example/vueCode.json";
-import { nuxtConfigCode } from "public/example/nuxtConfigCode.json";
-import { nuxtComponentCode } from "public/example/nuxtComponentCode.json";
+// import { nuxtConfigCode } from "public/example/nuxtConfigCode.json";
+// import { nuxtComponentCode } from "public/example/nuxtComponentCode.json";
 import { nuxtCode } from "public/example/nuxtCode.json";
 </script>
